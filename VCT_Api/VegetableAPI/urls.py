666d20ed8978207d5e775e2c585365b200c45139
kunni <aprkunni@gmail.com>
2022-06-17 17:38:20 +0900

    CRUD 구현

--- VCT_Api/VegetableAPI/urls.py
+++ VCT_Api/VegetableAPI/urls.py
@@ -4,4 +4,5 @@ from .views import UserView
 
 urlpatterns =[
     path('', UserView.as_view()),
+    path('<str:user_Id>', UserView.as_view())
 ]
--- VCT_Api/VegetableAPI/views.py
+++ VCT_Api/VegetableAPI/views.py
@@ -1,16 +1,48 @@
-import json
-from django.views import View
-from django.http import JsonResponse
-from VegetableAPI.models import User
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import UserSerializer
+from rest_framework import status
+from .models import User
 
 
-class UserView(View):
+class UserView(APIView):
     def post(self, request):
-        data = json.loads(request.body)
 
-        Users = User.objects.create(
-            user_Id=data["user_Id"],
-            user_Nick=data["user_Nick"],
-        )
+        user_serializer = UserSerializer(data=request.data)
+        if user_serializer.is_valid():
+            user_serializer.save()
+            return Response(user_serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response("Invaild Error", status=status.HTTP_400_BAD_REQUEST)
 
-        return JsonResponse({"message": "SUCCESS"}, status=201)
+    def get(self, request, **kwargs):
+        if kwargs.get('user_Id') is None:
+            return Response("Invaild Error", status=status.HTTP_400_BAD_REQUEST)
+        else:
+            user_id = kwargs.get('user_Id')
+            user_serializer = UserSerializer(User.objects.get(user_Id=user_id))
+            return Response(user_serializer.data, status.HTTP_200_OK)
+
+    def put(self, request, **kwargs):
+        if kwargs.get('user_Id') is None:
+            return Response("Invaild Request", status=status.HTTP_400_BAD_REQUEST)
+        else:
+            user_id = kwargs.get('user_Id')
+            user_object = User.objects.get(user_Id=user_id)
+
+            update_seriralizer = UserSerializer(user_object, data=request.data)
+            if update_seriralizer.is_valid():
+                update_seriralizer.save()
+                return Response(update_seriralizer.data, status=status.HTTP_200_OK)
+            else:
+                return Response("Invaild Request", status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, **kwargs):
+        if kwargs.get('user_Id') is None:
+            return Response("Invaild Error", status=status.HTTP_400_BAD_REQUEST)
+        else:
+            user_id = kwargs.get('user_Id')
+            user_object = User.objects.get(user_Id=user_id)
+            user_object.delete()
+            return Response("Delete", status=status.HTTP_200_OK)
+
