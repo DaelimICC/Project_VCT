02dc017a2a073b99261b9534e25d2a31ee25eac8
UBamtol <yij0325@gmail.com>
2022-07-18 23:58:29 +0900

    [ Web ] Add testAPI.json

--- VCT_Web/components/VegeIssue.jsx
+++ VCT_Web/components/VegeIssue.jsx
@@ -1,81 +1,36 @@
-import { useState } from 'react';
-import { getHTML } from '../pages/api/getHTML';
-
-// import getHTML from '../pages/api/getHTML';
-// const axios = require('axios');
-// const cheerio = require('cheerio');
-export const getStaticProps = async () => {
-  const datas = getHTML();
-  return {
-    props: {
-      datas,
-    },
-  };
-};
+import testAPI from '../pages/api/testAPI';
+import Image from 'next/image';
 
 const VegeIssue = () => {
-  getStaticProps().then((v) => console.log(v));
-  //   const [news, setNews] = useState('');
-  //   const getHTML = async () => {
-  //     try {
-  //       return await axios.get(
-  //         'https://search.naver.com/search.naver?where=news&ie=utf8&sm=nws_hty&query=%EC%95%BC%EC%B1%84'
-  //       );
-  //     } catch (error) {
-  //       console.error(error);
-  //     }
-  //   };
+  console.log(testAPI[0].image_url);
 
-  //   getHTML()
-  //     .then((html) => {
-  //       let ulList = [];
-  //       const $ = cheerio.load(html.data);
-  //       const bodyList = $('div.group_news ul.list_news li').children(
-  //         'li.bx div.news_wrap.api_ani_send'
-  //       );
-
-  //       bodyList.each(function (i, elem) {
-  //         ulList[i] = {
-  //           title: $(this).find('div.news_area a.news_tit').text(),
-  //           url: $(this).find('div.news_area a.news_tit').attr('href'),
-  //           media: $(this)
-  //             .find('div.news_area div.news_info div.info_group a.info.press')
-  //             .text(),
-  //           media_url: $(this)
-  //             .find(
-  //               'div.news_area div.news_info div.info_group a.info.press span.thumb_box img.thumb'
-  //             )
-  //             .attr('src'),
-  //           image_url: $(this).find('a.dsc_thumb img.thumb.api_get').attr('src'),
-  //           image_alt: $(this).find('a.dsc_thumb img.thumb.api_get').attr('alt'),
-  //           summary: $(this)
-  //             .find(
-  //               'div.news_area div.news_dsc div.dsc_wrap a.api_txt_lines.dsc_txt_wrap'
-  //             )
-  //             .text(),
-  //           // .slice(0, -11),
-  //           date: $(this)
-  //             .find('div.news_area div.news_info div.info_group span.info')
-  //             .text(),
-  //         };
-  //       });
-
-  //       const data = ulList.filter((n) => n.title);
-  //       return data;
-  //     })
-  //     .then((res) => {
-  //       console.log(res);
-  //       setNews(res);
-  //     });
-
-  // const { vegeNews } = getHTML();
   return (
     <div className="bg-white h-full">
       <div className="text-[18px] border-b border-gray-400 p-[20px]">
         야채 이슈
       </div>
-      <div className="border border-black p-[20px]">
-        <div></div>
+      <div className="flex border border-black p-[20px]">
+        <div>
+          <Image
+            src={testAPI[0].image_url}
+            alt={testAPI[0].image_alt}
+            width={80}
+            height={80}
+            className="rounded-lg"
+          />
+        </div>
+        <div>
+          <div className="flex">
+            <Image
+              src={testAPI[0].media_url}
+              alt="미디어"
+              width={12}
+              height={12}
+            />
+            <p>{testAPI[0].media}</p>
+          </div>
+          <div>{testAPI[0].title}</div>
+        </div>
       </div>
     </div>
   );
--- VCT_Web/next.config.js
+++ VCT_Web/next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ['search.pstatic.net'],
+  },
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
