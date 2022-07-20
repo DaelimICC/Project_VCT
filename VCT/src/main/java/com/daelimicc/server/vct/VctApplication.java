5989ff5371b58733c377211e08a536613d3dee46
Hod0ri <minthod0ri@gmail.com>
2022-07-20 16:45:43 +0900

    [ API ] Link Mongo-Server

--- VCT/src/main/java/com/daelimicc/server/vct/VctApplication.java
+++ VCT/src/main/java/com/daelimicc/server/vct/VctApplication.java
@@ -1,7 +1,9 @@
 package com.daelimicc.server.vct;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class VctApplication {
