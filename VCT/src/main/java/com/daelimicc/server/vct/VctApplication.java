bddceaad357eb22f239479d872826403b380f57e
Hod0ri <minthod0ri@gmail.com>
2022-07-20 15:20:20 +0900

    [ API ] Init Proejct (To Spring Boot)

---
+++ VCT/src/main/java/com/daelimicc/server/vct/VctApplication.java
@@ -0,0 +1,13 @@
+package com.daelimicc.server.vct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VctApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VctApplication.class, args);
+    }
+
+}
