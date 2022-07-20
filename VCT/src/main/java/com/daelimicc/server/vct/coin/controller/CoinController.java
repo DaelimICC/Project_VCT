90afbd8178b73929fec46381b63d83a13ace5e04
Hod0ri <minthod0ri@gmail.com>
2022-07-20 17:37:04 +0900

    [ API / DevOps ] MongoDB Dockerizing

--- VCT/src/main/java/com/daelimicc/server/vct/coin/controller/CoinController.java
+++ VCT/src/main/java/com/daelimicc/server/vct/coin/controller/CoinController.java
@@ -1,5 +1,6 @@
 package com.daelimicc.server.vct.coin.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/coin")
 public class CoinController {
 
+    // Test Method
+    @GetMapping("")
+    public String getCoin() {
+        return "coin";
+    }
 }
