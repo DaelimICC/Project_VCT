69134c300f4402a2743214225fbe7215ae662fdd
Hod0ri <minthod0ri@gmail.com>
2022-07-22 08:46:20 +0900

    [ API ] Solving Issue #3
    Jacoco Update

--- VCT/src/main/java/com/daelimicc/server/vct/mg/config/WebConfig.java
+++ VCT/src/main/java/com/daelimicc/server/vct/mg/config/WebConfig.java
@@ -19,7 +19,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").allowedOrigins("*").allowedMethods("*").allowedHeaders("*");
     }
-    
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new LoggerInterceptor())
