4d9a95961c624d80b717e1321f7736595f64e443
DESKTOP-KDPS2EK\Lee <aprkunni@gmail.com>
2022-07-21 19:04:45 +0900

    get 부분구현 delete, update 구현

--- VCT/src/main/java/com/daelimicc/server/vct/notice/controller/NoticeController.java
+++ VCT/src/main/java/com/daelimicc/server/vct/notice/controller/NoticeController.java
@@ -4,7 +4,12 @@ import com.daelimicc.server.vct.notice.domain.Notice;
 import com.daelimicc.server.vct.notice.dto.NoticeDTO;
 import com.daelimicc.server.vct.notice.repository.NoticeRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/notice")
@@ -16,6 +21,54 @@ public class NoticeController {
     @PostMapping("")
     public Notice recordNotice(@RequestBody NoticeDTO noticeDTO) {
         return noticeRepository.save(noticeDTO.toEntity());
+    }
+    //    @GetMapping("/all")
+//    public List<Notice> getCoinList() {
+//        return  noticeRepository.findAll(Sort.by(Sort.Direction.DESC,"noticeCrtDttm"));
+//    }
+
+
+//    @GetMapping("")
+//    public Notice getNoticeList(@RequestParam(value = "nt") String noticeTitle) {
+//        return noticeRepository.findByNoticeTitle(noticeTitle);
+//    }
+
+    @GetMapping("")
+    public List<Notice> getFindNotice(@RequestParam(required = false) String noticeTitle,
+                                      @RequestParam(required = false) String noticeContents) {
+
+        return noticeRepository.findByNoticeTitleOrNoticeContents(noticeTitle, noticeContents);
+
+    }
+//    @GetMapping("")
+//    public List<Notice> getFindNotice(@RequestParam(value = "nt", required = false) String noticeTitle) {
+//        return noticeRepository.findByNoticeTitle(noticeTitle);
+//    }
+
+        @PutMapping("")
+    public ResponseEntity<Notice> updateNotice(@RequestBody NoticeDTO noticeDTO, @RequestParam(value = "id") String _id) {
+
+        Notice notice = noticeDTO.toEntity();
+        notice.set_id(_id);
+        return new ResponseEntity<>(noticeRepository.save(notice), HttpStatus.OK);
+    }
+//    @PutMapping("/tutorials/{id}")
+//    public ResponseEntity<Notice> updateNotice(@PathVariable("id") String _id, @RequestBody Notice notice) {
+//        Optional<Notice> noticeData = noticeRepository.findById(_id);
+//        if (noticeData.isPresent()) {
+//            Notice _notice = noticeData.get();
+//            _notice.setNoticeTitle(notice.getNoticeTitle());
+//            _notice.setNoticeContents(notice.getNoticeContents());
+//            return new ResponseEntity<>(noticeRepository.save(_notice), HttpStatus.OK);
+//        } else {
+//            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+//        }
+//    }
+
+
+    @DeleteMapping("")
+    public List<Notice> deleteNotice(@RequestParam(value = "id") String _id){
+        return noticeRepository.deleteNoticeBy_id(_id);
     }
 
 }
--- VCT/src/main/java/com/daelimicc/server/vct/notice/repository/NoticeRepository.java
+++ VCT/src/main/java/com/daelimicc/server/vct/notice/repository/NoticeRepository.java
@@ -7,4 +7,12 @@ import java.util.List;
 
 public interface NoticeRepository extends MongoRepository<Notice, String> {
 
+    List<Notice> findByNoticeTitle(String noticeTitle);
+
+    List<Notice> findByNoticeTitleOrNoticeContents(String noticeTitle, String noticeContents);
+
+    List<Notice> findByNoticeTitleAndNoticeCrtDttm(String noticeTitle, String noticeCrtDttm);
+    List<Notice> deleteNoticeBy_id(String _id);
+
+
 }
