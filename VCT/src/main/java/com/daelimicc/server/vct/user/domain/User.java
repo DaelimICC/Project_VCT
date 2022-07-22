a60f4dcfb2eab291860d82fbf2fd5a341b81da4f
Hod0ri <minthod0ri@gmail.com>
2022-07-22 17:32:48 +0900

    [ API ] Implement User Domain (Temp)

--- VCT/src/main/java/com/daelimicc/server/vct/user/domain/User.java
+++ VCT/src/main/java/com/daelimicc/server/vct/user/domain/User.java
@@ -1,4 +1,16 @@
 package com.daelimicc.server.vct.user.domain;
 
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document(collection = "vct_user")
 public class User {
+    @Id
+    private String _id;
+    private String name;
+    private String email;
+    private String createdAt;
+    private String updatedAt;
 }
