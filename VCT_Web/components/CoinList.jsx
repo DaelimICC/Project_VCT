a6095df77953812130969188800d4170139bd38c
UBamtol <yij0325@gmail.com>
2022-07-23 23:57:09 +0900

    [ Web ] Fix CoinList Component

--- VCT_Web/components/CoinList.jsx
+++ VCT_Web/components/CoinList.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { StarIcon } from '@heroicons/react/solid';
 
 const CoinList = ({ type }) => {
-  console.log(type);
+  // console.log(type);
   const testList = [
     {
       coinName: '옥수수',
@@ -34,6 +34,18 @@ const CoinList = ({ type }) => {
       currentPrice: 900,
       range: -1.66,
     },
+    {
+      coinName: '고구마',
+      image: '🍠',
+      currentPrice: 900,
+      range: 1.66,
+    },
+    {
+      coinName: '고구마',
+      image: '🍠',
+      currentPrice: 900,
+      range: 1.66,
+    },
   ];
   return (
     <>
@@ -45,39 +57,41 @@ const CoinList = ({ type }) => {
           return type === 'rise' ? e.range > 0 : e.range < 0;
         })
         .map((v, i) => {
-          return (
-            <div className="flex w-full mt-1 px-4" key={i}>
-              <h3 className="flex p-3 justify-center items-center font-bold">
-                {i + 1}
-              </h3>
-              <div className="flex w-full justify-between">
-                <div className="flex">
-                  <div className="flex justify-center items-center ">
-                    <div className="w-[40px] h-[40px] flex p-3 justify-center items-center rounded-full bg-yellow-100">
-                      {v.image}
+          if (i < 5) {
+            return (
+              <div className="flex w-full mt-1 px-4" key={i}>
+                <h3 className="flex p-3 justify-center items-center font-bold">
+                  {i + 1}
+                </h3>
+                <div className="flex w-full justify-between">
+                  <div className="flex">
+                    <div className="flex justify-center items-center ">
+                      <div className="w-[40px] h-[40px] flex p-3 justify-center items-center rounded-full bg-yellow-100">
+                        {v.image}
+                      </div>
+                    </div>
+                    <div className="flex flex-col p-3 justify-center items-left">
+                      <h3 className="font-bold">{v.coinName}</h3>
+                      <h5 className="text-gray-500">
+                        {v.currentPrice.toLocaleString()}원
+                        <span
+                          className={`${
+                            v.range > 0 ? 'text-red-600' : 'text-blue-600'
+                          } font-bold`}
+                        >
+                          {v.range > 0 && '+'}
+                          {v.range}%
+                        </span>
+                      </h5>
                     </div>
                   </div>
-                  <div className="flex flex-col p-3 justify-center items-left">
-                    <h3 className="font-bold">{v.coinName}</h3>
-                    <h5 className="text-gray-500">
-                      {v.currentPrice.toLocaleString()}원
-                      <span
-                        className={`${
-                          v.range > 0 ? 'text-red-600' : 'text-blue-600'
-                        } font-bold`}
-                      >
-                        {v.range > 0 && '+'}
-                        {v.range}%
-                      </span>
-                    </h5>
+                  <div className="flex items-center text-gray-400 cursor-pointer hover:text-black">
+                    <StarIcon className="w-[20px] h-[20px]" />
                   </div>
                 </div>
-                <div className="flex items-center text-gray-400 cursor-pointer hover:text-black">
-                  <StarIcon className="w-[20px] h-[20px]" />
-                </div>
               </div>
-            </div>
-          );
+            );
+          }
         })}
     </>
   );
