3ad0e10b6aff579e92ed9ece29b2fe443581ac3f
Hod0ri <minthod0ri@gmail.com>
2022-07-25 08:49:19 +0900

    [ API ] Implement User Handling

--- VCT/src/main/java/com/daelimicc/server/vct/user/controller/UserController.java
+++ VCT/src/main/java/com/daelimicc/server/vct/user/controller/UserController.java
@@ -6,10 +6,7 @@ import com.daelimicc.server.vct.user.repository.UserRepository;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
@@ -27,6 +24,48 @@ public class UserController {
     @ApiOperation(value = "사용자 생성")
     @PostMapping("")
     public User registerUser(@RequestBody UserDTO userDTO) {
+        return userRepository.save(userDTO.toEntity());
+    }
+
+    /**
+     * 사용자 조회
+     * @param id
+     * @return
+     */
+    @ApiOperation(value = "사용자 정보 불러오기")
+    @GetMapping("")
+    public User getUser(@RequestParam String id) {
+        return userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("사용자가 존재하지 않습니다."));
+    }
+
+    /**
+     * 사용자 리스트 호출
+     * @return
+     */
+    @ApiOperation(value = "사용자 리스트 불러오기")
+    @GetMapping("/list")
+    public Iterable<User> getUserList() {
+        return userRepository.findAll();
+    }
+
+    /**
+     * 사용자 정보 삭제
+     * @param id
+     */
+    @ApiOperation(value = "사용자 삭제")
+    @DeleteMapping("")
+    public void deleteUser(@RequestParam String id) {
+        userRepository.deleteById(id);
+    }
+
+    /**
+     * 사용자 정보 수정
+     * @param userDTO
+     * @return
+     */
+    @ApiOperation(value = "사용자 정보 수정")
+    @PutMapping("")
+    public User updateUser(@RequestBody UserDTO userDTO) {
         return userRepository.save(userDTO.toEntity());
     }
 }
