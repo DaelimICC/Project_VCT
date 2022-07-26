26b3a2cecc2da50efaa087995854cda8057c5346
Hod0ri <minthod0ri@gmail.com>
2022-07-26 14:02:22 +0900

    [ API ] Set Scheduled

--- VCT/src/main/java/com/daelimicc/server/vct/mg/common/handler/WebSocketHandler.java
+++ VCT/src/main/java/com/daelimicc/server/vct/mg/common/handler/WebSocketHandler.java
@@ -1,17 +1,20 @@
 package com.daelimicc.server.vct.mg.common.handler;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import java.io.IOException;
+
+@Slf4j
 @Component
 public class WebSocketHandler extends TextWebSocketHandler {
-
-
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-        String payload = message.getPayload();
-        session.sendMessage(new TextMessage(payload));
+        String coinList = message.getPayload();
+        
     }
 }
