5e422bf50aafca018872155e3d22e683a698093f
Hod0ri <minthod0ri@gmail.com>
2022-07-26 15:39:38 +0900

    [ API ] Implement Python Script Scheduler

--- VCT/src/main/java/com/daelimicc/server/vct/mg/common/handler/WebSocketHandler.java
+++ VCT/src/main/java/com/daelimicc/server/vct/mg/common/handler/WebSocketHandler.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
@@ -17,30 +18,32 @@ import java.util.TimerTask;
 @Component
 @EnableScheduling
 public class WebSocketHandler extends TextWebSocketHandler {
+    // NOT USED
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-        String coinList = message.getPayload();
-
+        log.info(message.getPayload());
     }
 
+    // Chart Socket (Period 1sec)
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
         session.sendMessage(new TextMessage("Connected"));
-
-        TimerTask task = new TimerTask() {
-            public void run() {
-                try {
-                    session.sendMessage(new TextMessage("Temp Message"));
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
+        try {
+            TimerTask task = new TimerTask() {
+                public void run() {
+                    try {
+                        session.sendMessage(new TextMessage("Temp Message"));
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
                 }
-            }
-        };
-        Timer timer = new Timer("CharThreadTimer");
-        long delay = 3000L;
-        long period = 1000L;
-        timer.schedule(task, delay, period);
+            };
+            Timer timer = new Timer("ChartThreadTimer");
+            long delay = 3000L;
+            long period = 1000L;
+            timer.schedule(task, delay, period);
+        } catch (IllegalStateException e) {
+            throw new IllegalStateException(e);
+        }
     }
-
-
 }
---
+++ VCT/src/main/java/com/daelimicc/server/vct/mg/common/utility/PythonScriptComponent.java
@@ -0,0 +1,18 @@
+package com.daelimicc.server.vct.mg.common.utility;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PythonScriptComponent {
+
+//    @Scheduled(cron = "0 0 0 * * *")
+//    public void loadRealPriceData() {
+//        Process process = Runtime.getRuntime().exec();
+//    }
+//
+//    @Scheduled(cron = "0 */5 * * * *")
+//    public void CreateNewCoinData() {
+//        Process process = Runtime.getRuntime().exec();
+//    }
+}
