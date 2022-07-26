2680b46543106a2a90903bb8e7678609b714719b
Hod0ri <minthod0ri@gmail.com>
2022-07-26 15:55:58 +0900

    [ API ] Implement Load Coin data using Socket

--- VCT/src/main/java/com/daelimicc/server/vct/mg/common/handler/WebSocketHandler.java
+++ VCT/src/main/java/com/daelimicc/server/vct/mg/common/handler/WebSocketHandler.java
@@ -1,23 +1,32 @@
 package com.daelimicc.server.vct.mg.common.handler;
 
+import com.daelimicc.server.vct.coin.domain.Coin;
+import com.daelimicc.server.vct.coin.repository.CoinRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 import java.io.IOException;
-import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
 @Slf4j
 @Component
 @EnableScheduling
+@RequiredArgsConstructor
 public class WebSocketHandler extends TextWebSocketHandler {
+
+    private final CoinRepository coinRepository;
+
+    public List<Coin> getCoinChartList() {
+        return coinRepository.findAll();
+    }
+
     // NOT USED
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
@@ -32,7 +41,7 @@ public class WebSocketHandler extends TextWebSocketHandler {
             TimerTask task = new TimerTask() {
                 public void run() {
                     try {
-                        session.sendMessage(new TextMessage("Temp Message"));
+                        session.sendMessage(new TextMessage(getCoinChartList().toString()));
                     } catch (IOException e) {
                         throw new RuntimeException(e);
                     }
