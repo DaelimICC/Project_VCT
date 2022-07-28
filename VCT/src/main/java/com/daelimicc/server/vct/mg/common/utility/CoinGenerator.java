01ad161bf2d1e28f2c2ff0fb565b5853ecad4262
Hod0ri <minthod0ri@gmail.com>
2022-07-28 09:34:47 +0900

    [ API ] Migrate to Java

--- VCT/src/main/java/com/daelimicc/server/vct/mg/common/utility/CoinGenerator.java
+++ VCT/src/main/java/com/daelimicc/server/vct/mg/common/utility/CoinGenerator.java
@@ -3,7 +3,9 @@ package com.daelimicc.server.vct.mg.common.utility;
 import com.daelimicc.server.vct.coin.repository.CoinHistoryRepository;
 import com.daelimicc.server.vct.coin.repository.CoinRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 
+@Component
 @RequiredArgsConstructor
 public class CoinGenerator {
     private CoinRepository coinRepository;
