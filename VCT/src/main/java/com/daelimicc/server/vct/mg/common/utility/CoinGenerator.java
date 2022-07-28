24ecd3d358372545fd5c3b3fccdcfd79b2310bbf
Hod0ri <minthod0ri@gmail.com>
2022-07-28 09:19:02 +0900

    [ API ] Branching!

---
+++ VCT/src/main/java/com/daelimicc/server/vct/mg/common/utility/CoinGenerator.java
@@ -0,0 +1,11 @@
+package com.daelimicc.server.vct.mg.common.utility;
+
+import com.daelimicc.server.vct.coin.repository.CoinHistoryRepository;
+import com.daelimicc.server.vct.coin.repository.CoinRepository;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class CoinGenerator {
+    private CoinRepository coinRepository;
+    private CoinHistoryRepository coinHistoryRepository;
+}
