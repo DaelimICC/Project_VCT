14e3fade9a3315ac662f4eca2ea3854ea9e87138
Hod0ri <minthod0ri@gmail.com>
2022-07-29 16:05:13 +0900

    [ API ] Set Timezone

--- VCT/src/main/java/com/daelimicc/server/vct/VctApplication.java
+++ VCT/src/main/java/com/daelimicc/server/vct/VctApplication.java
@@ -6,9 +6,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @EnableScheduling
 @SpringBootApplication
 public class VctApplication {
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(VctApplication.class, args);
