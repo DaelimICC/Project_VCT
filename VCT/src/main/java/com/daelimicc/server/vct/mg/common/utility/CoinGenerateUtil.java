68caf20c6d4b70d321f4aa22b45e924dcb066079
hod0ri <minthod0ri@gmail.com>
2022-08-19 02:31:46 +0900

    [ API ] Temp Delete Utility

--- VCT/src/main/java/com/daelimicc/server/vct/mg/common/utility/CoinGenerateUtil.java
+++ VCT/src/main/java/com/daelimicc/server/vct/mg/common/utility/CoinGenerateUtil.java
@@ -2,6 +2,7 @@ package com.daelimicc.server.vct.mg.common.utility;
 
 import com.daelimicc.server.vct.coin.domain.CoinHistory;
 import com.daelimicc.server.vct.coin.repository.CoinHistoryRepository;
+import com.daelimicc.server.vct.mg.chart.repository.CandleChartRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -11,9 +12,22 @@ public class CoinGenerateUtil {
     @Autowired
     private CoinHistoryRepository coinHistoryRepository;
 
+    @Autowired
+    private CandleChartRepository candleChartRepository;
+
     public float getExDatePrice(String coinName) {
         CoinHistory ex = coinHistoryRepository.findTop1ByCoinNameOrderByCrtDttmDesc(coinName);
         return ex.getCoinPrice();
     }
+
+//    public float openPrice(String coinName) {
+//        // TODO: 오픈 가격을 가져오는 로직을 추가해야함.
+//        return 0;
+//    }
+//
+//    public float closePrice(String coinName) {
+//        // TODO : 종가 가격을 가져오는 로직 추가
+//        return 0;
+//    }
 
 }
